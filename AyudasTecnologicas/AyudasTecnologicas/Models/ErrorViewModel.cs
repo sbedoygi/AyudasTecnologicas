e00a677513ccd29d7b9742b1d53d7c79134e8666
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-05-21 11:06:44 -0500

    created your  application wit ASP.NetCore in visual studio

---
+++ AyudasTecnologicas/AyudasTecnologicas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AyudasTecnologicas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
