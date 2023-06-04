900eb695254a78bade80d3163a8f61688f8ca146
SEBASTIAN BEDOYA GIL <sebastianbedoya151125@correo.itm.edu.co>
2023-06-04 14:27:40 -0500

    ViewData  folderAccount

--- AyudasTecnologicas/AyudasTecnologicas/Models/AddProductViewModel.cs
+++ AyudasTecnologicas/AyudasTecnologicas/Models/AddProductViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace AyudasTecnologicas.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AyudasTecnologicas.Models
 {
-    public class AddProductViewModel
+    public class AddProductViewModel : EditTechnicalServicesViewModel
     {
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Guid CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        [Display(Name = "Foto")]
+        public IFormFile? ImageFile { get; set; }
     }
 }
